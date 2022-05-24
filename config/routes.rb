e3b76f6c16cb41915bc8d63f08d630f03a73d13b
Bondok6 <kirolousbondok6@gmail.com>
2022-05-24 15:16:59 +0200

    Add Navbar & Flash messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
   devise_for :users
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+
+
   root to: "home#index"
 end
