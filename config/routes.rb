1ad2085beebb78f8d1ef9f637111743de6f44b57
Bondok6 <kirolousbondok6@gmail.com>
2022-05-25 04:48:46 +0200

    Finish Authentication with Design

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
-  root to: 'home#index'
+  authenticated :user do
+    root to: 'groups#index', as: :authenticated_root
+  end
+  root to: 'splash#index'
 end
