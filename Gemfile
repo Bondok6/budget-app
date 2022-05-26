5df24177740d9263df21c01b6e0d0bc951e734a5
Bondok6 <kirolousbondok6@gmail.com>
2022-05-26 11:37:33 +0200

    Add test to login page

--- Gemfile
+++ Gemfile
@@ -14,9 +14,12 @@ gem 'sprockets-rails'
 gem 'stimulus-rails'
 gem 'turbo-rails'
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
+gem 'ffi'
 
 group :development, :test do
   gem 'debug', platforms: %i[mri mingw x64_mingw]
+  gem 'rails-controller-testing'
+  gem 'rspec-rails', '~> 6.0.0.rc1'
 end
 
 group :development do
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -51,6 +51,8 @@ Rails.application.configure do
   # Raise exceptions for disallowed deprecations.
   config.active_support.disallowed_deprecation = :raise
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
 
